b46c1ea62bbcc2dd3740543fb4c97f60ba109c87
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-02 06:01:18 +0800

    Update 123.py

--- bash/123.py
+++ bash/123.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     file_path = './feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm'
     line_num = 712
     new_line = '<tr style="background-color: #0d1117;"><td width="33%"><span style="color:#3fb937;">云编译作者：</a>  </td><td><a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">Joshua-DinG</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a><span style="color:#3fb937;">QQ:1009721777</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">社区</a>\n'
-    insert_line(file_path, line_num+2, new_line)
+    insert_line(file_path, line_num, new_line)
