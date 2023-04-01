4b053864b6d416a139260f21176382135e7263f3
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-02 01:03:23 +0800

    Update author.py

--- bash/author.py
+++ bash/author.py
@@ -3,10 +3,10 @@ if __name__ == '__main__':
     file_path = './feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm'
     line_num = 708
     new_line = '<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></span><span style="color:#00FF00;"> Plus</span></td></tr>\n'
-    modify_html(file_path, line_num, new_line)
+    index_html(file_path, line_num, new_line)
 
     # 示例2：在第712行后插入一行
     file_path = './feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm'
     line_num = 712
     new_line = '<tr style="background-color: #0d1117;"><td width="33%"><span style="color:#3fb937;">云编译作者：</a>  </td><td><a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">Joshua-DinG</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a><span style="color:#3fb937;">QQ:1009721777</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">社区</a>\n'
-    modify_html(file_path, line_num, new_line)
+    index_html(file_path, line_num, new_line)
