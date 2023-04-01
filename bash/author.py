69360d22548c93468d32c07f197b3d25cc629330
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-02 01:47:04 +0800

    Update author.py

--- bash/author.py
+++ bash/author.py
@@ -8,17 +8,15 @@ def replace_line(file_path, line_num, new_line):
         for line in lines:
             f.write(line)
 
-
 def insert_line(file_path, line_num, new_line):
     with open(file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
-    lines.insert(line_num, new_line)
+    lines.insert(line_num-1, new_line)
 
     with open(file_path, 'w', encoding='utf-8') as f:
         for line in lines:
             f.write(line)
-
 
 if __name__ == '__main__':
     # 示例1：替换第708行
@@ -31,4 +29,4 @@ if __name__ == '__main__':
     file_path = './feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm'
     line_num = 712
     new_line = '<tr style="background-color: #0d1117;"><td width="33%"><span style="color:#3fb937;">云编译作者：</a>  </td><td><a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">Joshua-DinG</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a><span style="color:#3fb937;">QQ:1009721777</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">社区</a>\n'
-    insert_line(file_path, line_num+1, new_line)
+    insert_line(file_path, line_num, new_line)
