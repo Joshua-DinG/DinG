69cecd9a9282523be81c5f1a92583db86ec7278c
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-02 01:56:55 +0800

    Update author.py

--- bash/author.py
+++ bash/author.py
@@ -2,22 +2,21 @@ def insert_line(file_path, line_num, new_line):
     with open(file_path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
 
-    lines.insert(line_num, new_line)
+    lines.insert(line_num, new_line + '\n')
 
     with open(file_path, 'w', encoding='utf-8') as f:
         for line in lines:
             f.write(line)
 
-
 if __name__ == '__main__':
     # 示例1：替换第708行
     file_path = './feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm'
     line_num = 708
-    new_line = '<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></span><span style="color:#00FF00;"> Plus</span></td></tr>\n'
+    new_line = '<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></span><span style="color:#00FF00;"> Plus</span></td></tr>'
     replace_line(file_path, line_num, new_line)
 
     # 示例2：在第712行后插入一行
     file_path = './feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm'
-    line_num = 711  # 由于列表下标从0开始，因此插入位置为原来的第712行，即列表下标为711
-    new_line = '<tr style="background-color: #0d1117;"><td width="33%"><span style="color:#3fb937;">云编译作者：</a>  </td><td><a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">Joshua-DinG</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a><span style="color:#3fb937;">QQ:1009721777</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">社区</a>\n'
+    line_num = 712
+    new_line = '<tr style="background-color: #0d1117;"><td width="33%"><span style="color:#3fb937;">云编译作者：</a>  </td><td><a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">Joshua-DinG</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a><span style="color:#3fb937;">QQ:1009721777</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">社区</a>'
     insert_line(file_path, line_num+1, new_line)
