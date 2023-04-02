5c620cf68e25019b6a741887196794f6ae9e769d
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-02 19:30:55 +0800

    Update diy-scripts.sh

--- bash/diy-scripts.sh
+++ bash/diy-scripts.sh
@@ -4,11 +4,11 @@ device=$1
 ui=$2
 diy=$3
 if [ "$device" = "Build-x86" ]; then
-  echo "$(cat $github_root/firmware/X86 $github_root/firmware/Generic)" >> .config
+  echo "$(cat "$GITHUB_WORKSPACE/firmware/X86" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
 elif [ "$device" = "Build-R2S" ]; then
-   echo "$(cat $github_root/firmware/R2S $github_root/firmware/Generic)" >> .config
+   echo "$(cat "$GITHUB_WORKSPACE/firmware/R2S" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
 elif [ "$device" = "Build-R4S" ]; then
-  echo "$(cat $github_root/firmware/R4S $github_root/firmware/Generic)" >> .config
+  echo "$(cat "$GITHUB_WORKSPACE/firmware/R4S" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
 fi
 
 if [ "$ui" = "true" ]; then
