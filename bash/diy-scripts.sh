5e7cdd05524588845370921b16ef2a083dba5c67
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-03 07:31:17 +0800

    Update diy-scripts.sh

--- bash/diy-scripts.sh
+++ bash/diy-scripts.sh
@@ -4,24 +4,9 @@ device=$2
 
 if [ "$1" = "diy" ]; then
 cat << EOF | sh
-# Uncomment a feed source
-##sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-# LINUX_VERSION
-#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(LINUX_VERSION)-/g' include/image.mk
-#
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile 
 sed -i 's/os.date(/&"%Y-%m-%d %H:%M:%S"/' package/lean/autocore/files/x86/index.htm
-# 关闭串口跑码
-#sed -i 's/console=tty0//g'  target/linux/x86/image/Makefile
-#sed -i 's/%V, %C/[Year] | by Jason /g' package/base-files/files/etc/banner
-#sed -i "s/Year/$(TZ=':Asia/Shanghai' date '+%Y')/g" package/base-files/files/etc/banner
 sed -i -e '$i\' -e "s/Year/$(TZ=':Asia/Shanghai' date '+%Y')/g" package/base-files/files/etc/banner
-#sed -i '/logins./a\                                          by Jason' package/base-files/files/etc/profile
-# Modify default passwd
-#sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
-# ID
-#sed -i "s/DISTRIB_REVISION='R.*.*.[0-9]/& Compiled by Jason/" package/lean/default-settings/files/zzz-default-settings
 rm -rf ./package/base-files/files/etc/banner
 rm -rf ./feeds/luci/themes
 rm -rf ./feeds/luci/applications/luci-app-argon-config
@@ -32,14 +17,27 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git ./package/lean/
 curl -f -L https://github.com/Joshua-DinG/Build-OpenWRT/raw/main/firmware/banner/banner -o ./package/base-files/files/etc/banner
 curl -f -L https://github.com/Joshua-DinG/Build-OpenWRT/raw/main/argon/bg1.jpg -o ./package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 curl https://raw.githubusercontent.com/Joshua-DinG/DinG/main/bash/123.py | python3 -
-#svn co https://github.com/Joshua-DinG/Build-OpenWRT/trunk/firmware/banner ./package/base-files/files/etc/
-#rm -rf ./package/base-files/files/etc/.svn/
 svn co https://github.com/Joshua-DinG/Build-OpenWRT/trunk/argon/video/default ./package/luci-theme-argon/htdocs/luci-static/argon/background/
 rm -rf ./package/luci-theme-argon/htdocs/luci-static/argon/background/.svn/
-# 临时
 EOF
 fi
 
+# Uncomment a feed source
+##sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+# LINUX_VERSION
+#sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(LINUX_VERSION)-/g' include/image.mk
+# 关闭串口跑码
+#sed -i 's/console=tty0//g'  target/linux/x86/image/Makefile
+#sed -i 's/%V, %C/[Year] | by Jason /g' package/base-files/files/etc/banner
+#sed -i "s/Year/$(TZ=':Asia/Shanghai' date '+%Y')/g" package/base-files/files/etc/banner
+#sed -i '/logins./a\                                          by Jason' package/base-files/files/etc/profile
+# Modify default passwd
+#sed -i '/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./ d' package/lean/default-settings/files/zzz-default-settings
+# ID
+#sed -i "s/DISTRIB_REVISION='R.*.*.[0-9]/& Compiled by Jason/" package/lean/default-settings/files/zzz-default-settings
+#svn co https://github.com/Joshua-DinG/Build-OpenWRT/trunk/firmware/banner ./package/base-files/files/etc/
+#rm -rf ./package/base-files/files/etc/.svn/
 
 
 if [ "$device" = "Build-x86" ]; then
