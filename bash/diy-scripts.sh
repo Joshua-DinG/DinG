be8737c51075b215e9af3e7e20c509505b9ef530
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-03 02:46:46 +0800

    Update diy-scripts.sh

--- bash/diy-scripts.sh
+++ bash/diy-scripts.sh
@@ -1,32 +1,16 @@
 #!/bin/sh
 
-device=$1
-core=$2
-diy=$5
-if [ "$device" = "Build-x86" ]; then
-  echo "$(cat "$GITHUB_WORKSPACE/firmware/X86" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
-elif [ "$device" = "Build-R2S" ]; then
-   echo "$(cat "$GITHUB_WORKSPACE/firmware/R2S" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
-elif [ "$device" = "Build-R4S" ]; then
-  echo "$(cat "$GITHUB_WORKSPACE/firmware/R4S" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
-fi
-
-if [ "$core" = "true" ]; then
-  sed -i s/5.15/6.1/g  ./target/linux/x86/Makefile
-fi
+device=$2
 
 if [ "$diy" = "diy" ]; then
 cat << EOF | sh
   # Uncomment a feed source
   ##sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
   #sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-
   # LINUX_VERSION
   #sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(LINUX_VERSION)-/g' include/image.mk
   #
-  sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-  
-  
+  sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile 
   sed -i 's/os.date(/&"%Y-%m-%d %H:%M:%S"/' package/lean/autocore/files/x86/index.htm
   # 关闭串口跑码
   #sed -i 's/console=tty0//g'  target/linux/x86/image/Makefile
@@ -54,18 +38,30 @@ cat << EOF | sh
 EOF
 fi
 
-# Check if input variable is set, else set to default value
+
+
+if [ "$device" = "Build-x86" ]; then
+  echo "$(cat "$GITHUB_WORKSPACE/firmware/X86" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
+elif [ "$device" = "Build-R2S" ]; then
+   echo "$(cat "$GITHUB_WORKSPACE/firmware/R2S" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
+elif [ "$device" = "Build-R4S" ]; then
+  echo "$(cat "$GITHUB_WORKSPACE/firmware/R4S" "$GITHUB_WORKSPACE/firmware/Generic")" >> ./.config
+fi
+
+file_name3="DinG"
 if [ -n "$3" ]; then
-  file_name="$3"
+  file_name3="$3"
+  sed -i "5i uci set system.@system[0].hostname=${file_name3}" package/lean/default-settings/files/zzz-default-settings
 fi
 
-# Create file with input file name
-sed -i '5i uci set system.@system[0].hostname=${file_name}' package/lean/default-settings/files/zzz-default-settings
-
-# Check if input variable is set, else set to default value
+file_name4="10.8.8.8"
 if [ -n "$4" ]; then
-  file_name="$4"
+  file_name4="$4"
+  sed -i "s/192.168.1.1/${file_name4}/g" package/base-files/files/bin/config_generate
 fi
 
-# Create file with input file name
-sed -i 's/192.168.1.1/${file_name}/g' package/base-files/files/bin/config_generate
+
+if [ "$5" = "true" ]; then
+  sed -i s/5.15/6.1/g  ./target/linux/x86/Makefile
+fi
+
