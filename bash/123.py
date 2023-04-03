ffbe71539c7a9b9693a7753336b27aac6d418fca
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-03 19:11:39 +0800

    Update 123.py

--- bash/123.py
+++ bash/123.py
@@ -22,13 +22,13 @@ def insert_line(file_path, line_num, new_line):
 
 if __name__ == '__main__':
     # 示例1：替换第708行
-    file_path = './feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm'
-    line_num = 708
+    file_path = './package/lean/autocore/files/x86/index.htm'
+    line_num = 746
     new_line = '<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></span><span style="color:#00FF00;"> Plus</span></td></tr>\n'
     replace_line(file_path, line_num, new_line)
 
     # 示例2：在第713行插入一行
-    file_path = './feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm'
-    line_num = 712
+    file_path = './package/lean/autocore/files/x86/index.htm'
+    line_num = 750
     new_line = '<tr style="background-color: #0d1117;"><td width="33%"><span style="color:#3fb937;">云编译作者：</a>  </td><td><a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">Joshua-DinG</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a><span style="color:#3fb937;">QQ:1009721777</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">社区</a>\n'
     insert_line(file_path, line_num, new_line)
