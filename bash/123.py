c61a764eec2e8357cc66a39ddfbf5c5a1d4fd3bb
DinG <96579381+Joshua-DinG@users.noreply.github.com>
2023-04-08 01:29:12 +0800

    Update 123.py

--- bash/123.py
+++ bash/123.py
@@ -24,13 +24,13 @@ if __name__ == '__main__':
     # 示例1：替换第708行
     file_path = './package/lean/autocore/files/x86/index.htm'
     line_num = 746
-    new_line = '<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></span><span style="color:#00FF00;"> Plus</span></td></tr>\n'
+    new_line = '<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></span><span style="color:#3fb937;">&nbsp; Plus</span></td></tr>\n'
     replace_line(file_path, line_num, new_line)
 
     # 示例2：在第713行插入一行
     file_path = './package/lean/autocore/files/x86/index.htm'
     line_num = 750
-    new_line = '<tr style="background-color: #0d1117;"><td width="33%"><span style="color:#3fb937;">Nice To Meet You !</a>  </td><td><a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">Joshua-DinG</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a><span style="color:#3fb937;">GitHub &nbsp; DinG</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">机场</a>\n'
+    new_line = '<tr style="background-color: #0d1117;"><td width="33%"><span style="color:#3fb937;">Nice To Meet You !</a>  </td><td><a href="https://github.com/Joshua-DinG/Build-OpenWRT"><span style="color:#3fb937;">Joshua-DinG&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;QQ:1009721777</a></td></tr>\n'
     insert_line(file_path, line_num+1, new_line)
 
     insert_line(file_path, line_num, new_line)
